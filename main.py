586ad814572622470bb1110e12e097ace79a0262
Shadiq Harwiz <13520038@std.stei.itb.ac.id>
2023-09-11 14:06:43 +0700

    implement generate image function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+from function.generateImage import *
+
+def main() :
+  objectClassDictionary = {
+    0: 'bear',
+    1: 'panda'
+  }
+  dataInput, dataClassLabel = generateImage()
+
+  print("Data yang diperoleh dari generate image")
+  print(dataInput[0])
+  print("===========")
+
+main()
