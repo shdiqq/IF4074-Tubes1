c5fca4ca0fa4a6b91de26df867c7caa609377383
Shadiq Harwiz <13520038@std.stei.itb.ac.id>
2023-09-12 18:59:19 +0700

    add PoolingStage to ConvolutionalLayer

--- class/layer/ConvolutionalLayer.py
+++ class/layer/ConvolutionalLayer.py
@@ -8,13 +8,16 @@ sys.path.append(mymodule_dir)
 
 from ConvolutionalStage import ConvolutionalStage
 from DetectorStage import DetectorStage
+from PoolingStage import PoolingStage
 
 class ConvolutionalLayer():
-  def __init__(self, filterSize, numFilter,  numDepth, padding = 0, stride = 1):
+  def __init__(self, filterSize, numFilter, numDepth, mode, padding = 0, stride = 1):
     self.convolutionStage = ConvolutionalStage(filterSize, numFilter, numDepth, padding, stride)
     self.detectorStage = DetectorStage()
+    self.poolingStage = PoolingStage(filterSize, stride, mode)
 
   def forward(self, inputData):
     featureMap = self.convolutionStage.forward(inputData)
     outputDetector = self.detectorStage.forward(featureMap)
-    return outputDetector
+    outputPooling = self.poolingStage.forward(outputDetector)
+    return outputPooling
