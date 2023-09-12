8db906e01bfb83893a2c445975c475f910abac50
Shadiq Harwiz <13520038@std.stei.itb.ac.id>
2023-09-12 19:28:03 +0700

    spec adjust

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from function.generateImage import *
 
-import os
-import sys
-
-script_dir = os.path.dirname(__file__)
-mymodule_dir = os.path.join(script_dir, 'class')
-sys.path.append(mymodule_dir)
-
-# from layer.ConvolutionalLayer import ConvolutionalLayer
-from stage.ConvolutionalStage import ConvolutionalStage
-from stage.DetectorStage import DetectorStage
-from stage.PoolingStage import PoolingStage
-
-def main() :
+if __name__ == "__main__":
   objectClassDictionary = {
     0: 'bear',
     1: 'panda'
@@ -22,27 +10,3 @@ def main() :
   print("Data yang diperoleh dari generate image")
   print(dataInput[0].shape)
   print("===========")
-
-  cnn = ConvolutionalStage(filterSize = (3, 3), numFilter = 3, numDepth = dataInput.shape[3], padding = 1, stride = 1)
-  output = cnn.forward(dataInput[0])
-
-  print("Data yang diperoleh setelah convolutional stage")
-  print(output.shape)
-  print("===========")
-
-  detectorStage = DetectorStage()
-  outputDetector = detectorStage.forward(output)
-
-  print("Setelah detector stage")
-  print(outputDetector.shape)
-  print("===========")
-
-  poolingStage = PoolingStage(filterSize = 3, stride = 1, mode='max')
-  outputPooling = poolingStage.forward(output)
-
-  print("Setelah pooling stage")
-  print(outputPooling.shape)
-  print("===========")
-
-
-main()
