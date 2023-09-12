c889977013169832e941dbda92e1015835f0817a
Shadiq Harwiz <13520038@std.stei.itb.ac.id>
2023-09-12 18:55:49 +0700

    implement class pooling stage

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ sys.path.append(mymodule_dir)
 # from layer.ConvolutionalLayer import ConvolutionalLayer
 from stage.ConvolutionalStage import ConvolutionalStage
 from stage.DetectorStage import DetectorStage
+from stage.PoolingStage import PoolingStage
 
 def main() :
   objectClassDictionary = {
@@ -19,21 +20,28 @@ def main() :
   dataInput, dataClassLabel = generateImage()
 
   print("Data yang diperoleh dari generate image")
-  print(dataInput[0])
+  print(dataInput[0].shape)
   print("===========")
 
   cnn = ConvolutionalStage(filterSize = (3, 3), numFilter = 3, numDepth = dataInput.shape[3], padding = 1, stride = 1)
   output = cnn.forward(dataInput[0])
 
   print("Data yang diperoleh setelah convolutional stage")
-  print(output)
+  print(output.shape)
   print("===========")
 
   detectorStage = DetectorStage()
   outputDetector = detectorStage.forward(output)
 
   print("Setelah detector stage")
-  print(outputDetector)
+  print(outputDetector.shape)
+  print("===========")
+
+  poolingStage = PoolingStage(filterSize = 3, stride = 1, mode='max')
+  outputPooling = poolingStage.forward(output)
+
+  print("Setelah pooling stage")
+  print(outputPooling.shape)
   print("===========")
 
 
