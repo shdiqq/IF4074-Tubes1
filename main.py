5f4fd0f53d4631fbbd0ed3a306caae872f2e164a
irsyadazka <16520096@mahasiswa.itb.ac.id>
2023-09-13 18:20:19 +0700

    sequential?

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ mymodule_dir = os.path.join(script_dir, 'class')
 sys.path.append(mymodule_dir)
 
 from function.generateImage import *
-from layer.ConvolutionalLayer import ConvolutionalLayer
-from layer.DenseLayer import DenseLayer
-from layer.FlattenLayer import FlattenLayer
+from CNN import CNN
 
 if __name__ == "__main__":
   objectClassDictionary = {
@@ -17,27 +15,60 @@ if __name__ == "__main__":
   }
   dataInput, dataClassLabel = generateImage()
 
+  # dataInput = np.array(
+  #   [
+  #     [
+  #       [
+  #         [1,11,2],
+  #         [1,10,4],
+  #         [6,12,8],
+  #       ],
+  #       [
+  #         [7,1,2],
+  #         [5,-1,2],
+  #         [7,-4,2],
+  #       ],
+  #       [
+  #         [-2,23,2],
+  #         [2,20,4],
+  #         [8,6,6],
+  #       ]
+  #     ]
+  #   ]
+  # )
+
   print("Data yang diperoleh dari generate image")
-  print(dataInput[1].shape)
+  print(dataInput[0].shape)
   print("===========")
 
-  convolutionalLayer = ConvolutionalLayer(inputSize = dataInput[0].shape, filterSize = 16, numFilter = 3, mode='max', padding = 1, stride = 8)
-  newMatrix = convolutionalLayer.forward(dataInput[0])
+  cnn = CNN(dataInput[0])
+
+  cnn.addConvolutionalLayer(filterSize = 8, numFilter = 5, mode = 'max', padding = 2, stride = 8)
 
   print("Data yang diperoleh dari convolutional layer")
-  print(newMatrix.shape)
+  print(cnn.output.shape)
   print("===========")
 
-  flattenLayer = FlattenLayer()
-  newMatrix = flattenLayer.forward(newMatrix)
+  cnn.addConvolutionalLayer(filterSize = 8, numFilter = 15, mode = 'max', padding = 2, stride = 8)
+
+  print("Data yang diperoleh dari convolutional layer")
+  print(cnn.output.shape)
+  print("===========")
+
+  cnn.addFlattenLayer()
 
   print("Data yang diperoleh dari flatten layer")
-  print(newMatrix.shape)
+  print(cnn.output.shape)
   print("===========")
 
-  denseLayer = DenseLayer(newMatrix.shape[0], 125, 'relu')
-  newMatrix = denseLayer.forward(newMatrix)
+  cnn.addDenseLayer(outputSize = 16, activation = 'relu')
 
   print("Data yang diperoleh dari dense layer")
-  print(newMatrix.shape)
+  print(cnn.output)
+  print("===========")
+
+  cnn.addDenseLayer(outputSize = 4, activation = 'relu')
+
+  print("Data yang diperoleh dari dense layer")
+  print(cnn.output)
   print("===========")
